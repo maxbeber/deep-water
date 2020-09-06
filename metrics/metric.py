3937f4a30ec2cbb5d557e3c31f26c84366118054
maxbeber <max.beber@gmail.com>
2020-09-06 17:46:56 +0200

    Minor refactoring in the mean_iou function.

--- metrics/metric.py
+++ metrics/metric.py
@@ -13,9 +13,10 @@ def mean_iou(y_true, y_pred):
 
     Returns the computed metric as floating point 32 bytes.
     """
+    threshold = 0.5
     yt0 = y_true[:, :, :, 0]
-    yp0 = tf.keras.backend.cast(y_pred[:, :, :, 0] > 0.5, 'float32')
+    yp0 = tf.keras.backend.cast(y_pred[:, :, :, 0] > threshold, 'float32')
     intersection = tf.math.count_nonzero(tf.logical_and(tf.equal(yt0, 1), tf.equal(yp0, 1)))
     union = tf.math.count_nonzero(tf.add(yt0, yp0))
-    iou = tf.where(tf.equal(union, 0), 1., tf.cast(intersection/union, 'float32'))
+    iou = tf.where(tf.equal(union, 0), 1.0, tf.cast(intersection/union, 'float32'))
     return iou   
