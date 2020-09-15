e55a9b07ad25e3c2e925e8bac564cffa95abfdce
KarlRadtke <karl.radtke@uni-konstanz.de>
2020-09-15 21:10:42 +0200

    function to import the annotation of the respective lake

--- preprocessing/app_helpers.py
+++ preprocessing/app_helpers.py
@@ -3,6 +3,8 @@ import requests
 import shutil
 import rasterio
 import numpy as np
+import json
+import os
 
 #--------------------------------------------------------------------
 def download_image(data_frame, slct_lake):
@@ -66,3 +68,33 @@ def lat_long(data_frame, slct_lake):
         dff["max_latitude"]
     ]
     return longit, latit
+
+
+#--------------------------------------------------------------------   
+def import_annotation(data_frame, slct_lake):
+    dff = data_frame.loc[slct_lake, :]
+    query = dff["country"] + "_" +dff["name"]
+    
+    path = "annotations/s2cloudless"
+    files = os.listdir(path)
+    item = [query in i for i in files].index(True)
+    file_ = files[item]
+    query_path = os.path.join(path, file_)
+
+    with open(query_path) as f:
+        annotation = json.load(f)
+
+    im_name = list(annotation.keys())[0]
+
+
+    X = []
+    Y = []
+
+    for i in annotation[im_name]['regions']:
+        x = annotation[im_name]["regions"][i]["shape_attributes"]["all_points_x"]
+        y = annotation[im_name]["regions"][i]["shape_attributes"]["all_points_y"]
+        X.append(x)
+        Y.append(y)
+        assert len(X) == len(Y)
+
+    return X, Y
