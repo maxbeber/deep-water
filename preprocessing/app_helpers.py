42832be0ee12fdf069ea0427feee9878cc6fa568
maxbeber <max.beber@gmail.com>
2020-11-03 09:45:29 +0100

    Code clean up.

--- preprocessing/app_helpers.py
+++ preprocessing/app_helpers.py
@@ -1,21 +1,19 @@
+import json
+import numpy as np
+import os
 import pandas as pd
+import rasterio
 import requests
 import shutil
-import rasterio
-import numpy as np
-import json
-import os
 
-#--------------------------------------------------------------------
+
 def download_image(data_frame, slct_lake):
     dff = data_frame.loc[slct_lake, :]
-    
     min_longitude = dff['min_longitude']
     min_latitude = dff['min_latitude']
     max_longitude = dff['max_longitude']
     max_latitude = dff['max_latitude']
     bounding_box = f"{min_longitude},{min_latitude},{max_longitude},{max_latitude}"
-
     payload = {
         "version": '1.1.1',
         "layers": "s2cloudless-2019",
@@ -24,24 +22,18 @@ def download_image(data_frame, slct_lake):
         "srs": 'epsg:4326',
         "bbox": bounding_box
     }
-
     base_url = 'https://tiles.maps.eox.at/wms'
     base_url_parameters = '?service=wms&request=getmap'
-
     url = base_url + base_url_parameters
     r = requests.get(url, params=payload, stream=True)
-
     filename = "sample_image.jpg"
-
     if r.status_code == 200:
         # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
         r.raw.decode_content = True
-        
         with open(filename,'wb') as f:
             shutil.copyfileobj(r.raw, f)
 
 
-#--------------------------------------------------------------------
 def import_image():
     dataset = rasterio.open("sample_image.jpg")
     bands = dataset.read()
@@ -50,7 +42,6 @@ def import_image():
     return image
     
 
-#--------------------------------------------------------------------
 def lat_long(data_frame, slct_lake):
     dff = data_frame.loc[slct_lake, :]
 
@@ -70,7 +61,6 @@ def lat_long(data_frame, slct_lake):
     return longit, latit
 
 
-#--------------------------------------------------------------------   
 def import_annotation(data_frame, slct_lake):
     dff = data_frame.loc[slct_lake, :]
     query = dff["country"] + "_" +dff["name"]
@@ -85,7 +75,6 @@ def import_annotation(data_frame, slct_lake):
         annotation = json.load(f)
 
     im_name = list(annotation.keys())[0]
-
 
     X = []
     Y = []
