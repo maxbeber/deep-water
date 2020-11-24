4654230a22db3dd0ab38b5026b081904a27075b1
maxbeber <max.beber@gmail.com>
2020-11-24 18:34:22 +0100

    Minor modification.

--- models/callbacks.py
+++ models/callbacks.py
@@ -12,8 +12,8 @@ def build_callbacks(parameters):
     model_name = parameters['model_name']
     is_logger_enabled = parameters['enable_csv_logger']
     is_early_stopping_enabled = 'early_stopping' in parameters.keys()
-    is_reduce_lr_on_plateau_enabled = parameters['reduce_lr_on_plateau']
     is_plot_learning_enabled = 'plot_learning' in parameters.keys()
+    is_reduce_lr_on_plateau_enabled = parameters['reduce_lr_on_plateau']
     callbacks = model_checkpoint(model_name)
     if is_early_stopping_enabled:
         early_stop = early_stopping(parameters)
